5ff2702f02b5b264620b65a31792a2cf47babd1e
Mouhamad Sleimane <mohammad.sleimane@isae.edu.lb>
2026-04-08 21:18:33 +0300

    cors problem fixed

--- src/server.js
+++ src/server.js
@@ -3,7 +3,14 @@ const connection = require('./config/connection');
 const express=require('express');
 const app=express();
 const cors=require('cors');
-app.use(cors());
+app.use(cors({
+    origin: 'https://cards-nhsf.onrender.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
+
+app.options('*', cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
