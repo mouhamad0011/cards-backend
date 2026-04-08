11e343b9400f4b1f4d9b028e4071df766f5640ab
Mouhamad Sleimane <mohammad.sleimane@isae.edu.lb>
2026-04-08 21:23:41 +0300

    cors problem

--- src/server.js
+++ src/server.js
@@ -3,13 +3,14 @@ const connection = require('./config/connection');
 const express=require('express');
 const app=express();
 const cors=require('cors');
-app.use(cors({
-    origin: 'https://cards-nhsf.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+const corsOptions = {
+    origin: "https://cards-nhsf.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
-  }));
-
-app.options('*', cors());
+  };
+  
+  app.use(cors(corsOptions));
+  app.options("*", cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
